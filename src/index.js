16806b198d0acca3662f9b1ffccf0ae0a13f2dc9
Andrey Borodin <flyer29@bk.ru>
2020-07-13 11:21:09 +0300

    поправил запись условия протокола

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ import FormValidator from "./js/FormValidator.js";
   const imagePopupTemplate = document.querySelector('.popup_type_image');
   const avatatPopupTemplate = document.querySelector('.popup_type_avatar');
   const picture = document.querySelector('.user-info__photo');
-  const reference = process.env.NODE_ENV === 'development' ? 'http://praktikum.tk' : 'https://praktikum.tk';
+  const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
   const config = {
-    baseUrl: `${reference}/cohort11`,
+    baseUrl: `${protocol}://praktikum.tk/cohort11`,
     headers: {
       authorization: "6282d211-b1bd-4fad-af58-e65f4129d6a3",
       "Content-Type": "application/json"
